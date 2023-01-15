f75445a7ce21c73398fdce69d67634161cab15e5
Andrew Lundy <andrewlundy@icloud.com>
2023-01-15 12:06:38 -0600

    Add all files from greetings module

---
+++ greetings.go
@@ -0,0 +1,46 @@
+// greetings.go
+package greetings
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("the 'name' parameter was empty when the 'Hello' function was called")
+	}
+
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Guten tag, %v! Willkommen!",
+	}
+
+	return formats[rand.Intn(len(formats))]
+}
+
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		messages[name] = message
+	}
+
+	return messages, nil
+}
